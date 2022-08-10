75450d4aa4bfe84d8b8dd90d608bdf945a42feca
Iannrst <iannrst11@gmail.com>
2022-08-10 18:23:56 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
-
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -41,8 +42,10 @@ export default function App() {
   }
 
   return (
-    <View onLayout={onLayoutRootView}>
-      <Text>Olá</Text>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <ThemeProvider theme={theme}>
+        <Text>Olá</Text>
+      </ThemeProvider>
     </View>
   );
 }
