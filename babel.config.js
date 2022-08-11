2c5bfd668fa46b8dc97e6466c3378668addc8abe
Iannrst <iannrst11@gmail.com>
2022-08-11 15:12:28 -0300

    babel configuração

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,8 @@ module.exports = function (api) {
             '@components': './src/components',
             '@screens': './src/screens',
             '@assets': './src/assets',
+            '@hooks': './src/hooks',
+
           }
         }
       ]
