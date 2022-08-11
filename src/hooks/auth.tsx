dd9f2e0d2c0b7ed8ea284bfa27085e1e630a7a3c
Iannrst <iannrst11@gmail.com>
2022-08-11 15:42:44 -0300

    feat: autenticação

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,8 +1,10 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
-
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 type AuthContextData = {
-
+  signIn: (email: string, password: string) => Promise<void>;
+  isLogging: boolean;
 }
 
 type AuthProviderProps = {
@@ -12,8 +14,29 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
+  const [isLogging, setIsLogging] = useState(false);
+
+  async function signIn(email: string, password: string) {
+    if (!email || !password) {
+      return Alert.alert('Login', 'Informe o e-mail e a senha.');
+    }
+
+    setIsLogging(true);
+
+    auth().signInWithEmailAndPassword(email, password).then(account => {
+      console.log(account);
+    }).catch(error => {
+      const { code } = error;
+
+      if (code === 'auth/user-not-found' || code === 'auth/wrong-password') {
+        return Alert.alert('Login', 'E-mail e/ou senha inválida.');
+      } else {
+        return Alert.alert('Login', 'Não foi possível realizar o login.')
+      }
+    }).finally(() => setIsLogging(false))
+  }
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{ signIn, isLogging }}>
       {children}
     </AuthContext.Provider>
   )
