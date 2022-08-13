c222b438eba7c066a6eb3c0360f4b05ee55cc9d9
Iann-rst <iannrst11@gmail.com>
2022-08-13 15:42:32 -0300

    rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 import { NavigationContainer } from '@react-navigation/native';
 
+import { useAuth } from '@hooks/auth';
 import { UserStackRoutes } from './user.stack.routes';
+import { UserTabRoutes } from './user.tab.routes';
+import { SignIn } from '@screens/SignIn';
+
+
 
 export function Routes() {
+  const { user } = useAuth();
   return (
     <NavigationContainer>
-      <UserStackRoutes />
+      {user ? <UserStackRoutes /> : <SignIn />}
     </NavigationContainer>
   )
 }
