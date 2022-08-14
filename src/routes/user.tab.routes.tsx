f7d4ac315b8b85659c80b2279efc3056d4fde4ef
Iann-rst <iannrst11@gmail.com>
2022-08-14 10:58:29 -0300

    feat: notificação dos pedidos em aberto

--- src/routes/user.tab.routes.tsx
+++ src/routes/user.tab.routes.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Platform } from 'react-native';
 import { useTheme } from 'styled-components/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import firestore from '@react-native-firebase/firestore';
 
 import { Home } from '@screens/Home';
 import { Orders } from '@screens/Orders';
@@ -12,7 +14,21 @@ import { BottomMenu } from '@components/BottomMenu';
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function UserTabRoutes() {
+  const [notifications, setNotifications] = useState('0');
   const { COLORS } = useTheme();
+
+
+  useEffect(() => {
+    const subscribe = firestore().collection('orders').where('status', '==', 'Pronto').onSnapshot(querySnapshot => {
+      setNotifications(String(querySnapshot.docs.length));
+    })
+
+    return () => subscribe();
+  }, [])
+
+
+
+
   return (
     <Navigator screenOptions={{
       tabBarActiveTintColor: COLORS.SECONDARY_900,
@@ -40,7 +56,7 @@ export function UserTabRoutes() {
         component={Orders}
         options={{
           tabBarIcon: ({ color }) => (
-            <BottomMenu title="Pedidos" color={color} notifications="5" />
+            <BottomMenu title="Pedidos" color={color} notifications={notifications} />
           )
         }}
       />
